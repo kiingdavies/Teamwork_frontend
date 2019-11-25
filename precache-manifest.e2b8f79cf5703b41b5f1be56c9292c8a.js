8c1aab0737ffafcb3bd93bf5666232584f2fcd36
kiingdavies <daviesadedeji007@gmail.com>
2019-11-25 11:02:42 +0100

    Updates

---
+++ precache-manifest.e2b8f79cf5703b41b5f1be56c9292c8a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9480c9f6424985068a34db0000cfbaeb",
+    "url": "/Teamwork_frontend/index.html"
+  },
+  {
+    "revision": "21a863cc75b0e4d92a59",
+    "url": "/Teamwork_frontend/static/css/main.c4e8a27d.chunk.css"
+  },
+  {
+    "revision": "a51d2efdc71e3e92f3ab",
+    "url": "/Teamwork_frontend/static/js/2.b45918d1.chunk.js"
+  },
+  {
+    "revision": "21a863cc75b0e4d92a59",
+    "url": "/Teamwork_frontend/static/js/main.633a354e.chunk.js"
+  },
+  {
+    "revision": "e55d4596e699350bbe37",
+    "url": "/Teamwork_frontend/static/js/runtime-main.16ccd248.js"
+  }
+]);
